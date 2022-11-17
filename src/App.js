9642c617e0131d8857f1e4e25d04bb6a75571f4e
Youssef Ragab <ragab.youssef95@gmail.com>
2022-11-17 10:55:17 -0500

    refactor category menu

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-import "./categories.styles.scss";
+import CategoryMenu from "./components/category-menu/category-menu.component";
 
 const App = () => {
-  const categories = [
-    { id: 1, title: "Hats" },
-    { id: 2, title: "Jackets" },
-    { id: 3, title: "Sneakers" },
-    { id: 4, title: "Womens" },
-    { id: 5, title: "Mens" },
-  ];
-
-  return (
-    <div className="categories-container">
-      {categories.map(({ id, title }) => (
-        <div key={id} className="category-container">
-          <div className="background-image" />
-          {/* <img /> */}
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <CategoryMenu />;
 };
 
 export default App;
